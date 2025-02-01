e79769d654d58931cad50a6c792c9466a44854ee
flaukowski <39143452-flaukowski@users.noreply.replit.com>
2025-02-01 23:35:35 +0000

    Agent query: Is the Music Player now visible at the bottom of the page?
    Fix: Position MusicPlayer at the bottom of the screen
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/61713cfd-62aa-4231-bdd2-e42328d5516f/d9a3e77c-80db-447d-8789-ce294ac8d3da.jpg

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -37,7 +37,10 @@ export function Layout({ children }: LayoutProps) {
         {children}
       </main>
 
-      <MusicPlayer />
+      {/* Fixed Music Player at bottom */}
+      <div className="fixed bottom-0 left-0 right-0 z-50">
+        <MusicPlayer />
+      </div>
     </div>
   );
 }
